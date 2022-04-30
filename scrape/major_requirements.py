4deec58794fd7ff39be9278c11ee1daeaabb045b
Tram La <latb@uci.edu>
2022-04-29 19:27:59 -0700

    Add more majors to major_requirement

--- scrape/major_requirements.py
+++ scrape/major_requirements.py
@@ -30,37 +30,37 @@ def get_websites():
     """
     get_websites scrapes all the redirected websites in the main page of UCI major requirements.
     """
-    # major_names = []
-    # all_href = []
-    # prefered = ['bs/', 'ba/', 'bfa/']
-    # soup = request_websites("http://catalogue.uci.edu/undergraduatedegrees/")
-    # for elem in soup.find_all('ul'):
-    #     for each in elem.find_all('a'):
-    #         href = each.get('href')
-    #         get_type = href.split('_')
-    #         website = 'http://catalogue.uci.edu/' + href
-    #         name = each.text
-    #         if get_type[-1] in prefered and name not in major_names:
-    #             all_href.append([each.text, website])
-    #             major_names.append(name)
-
-    # return all_href
-
     major_names = []
     all_href = []
     prefered = ['bs/', 'ba/', 'bfa/']
     soup = request_websites("http://catalogue.uci.edu/undergraduatedegrees/")
-    find_div = soup.find_all('ul')
-    get_li = find_div[36].find_all('a')
-    for each in get_li:
-        href = each.get('href')
-        get_type = href.split('_')
-        website = 'http://catalogue.uci.edu/' + href
-        name = each.text
-        if get_type[-1] in prefered:
-            all_href.append([each.text, website])
-            major_names.append(name)
+    for elem in soup.find_all('ul'):
+        for each in elem.find_all('a'):
+            href = each.get('href')
+            get_type = href.split('_')
+            website = 'http://catalogue.uci.edu/' + href
+            name = each.text
+            if get_type[-1] in prefered:
+                all_href.append([each.text, website])
+                major_names.append(name)
+
     return all_href
+
+    # major_names = []
+    # all_href = []
+    # prefered = ['bs/', 'ba/', 'bfa/']
+    # soup = request_websites("http://catalogue.uci.edu/undergraduatedegrees/")
+    # find_div = soup.find_all('ul')
+    # get_li = find_div[36].find_all('a')
+    # for each in get_li:
+    #     href = each.get('href')
+    #     get_type = href.split('_')
+    #     website = 'http://catalogue.uci.edu/' + href
+    #     name = each.text
+    #     if get_type[-1] in prefered:
+    #         all_href.append([each.text, website])
+    #         major_names.append(name)
+    # return all_href
 
 
 def get_name(in_string):
@@ -110,52 +110,56 @@ def get_series(all_courses, in_string, name):
 
 
 def scrape_courses(url):
+
     soup = request_websites(url)
     table = soup.find('div', id='requirementstextcontainer')
     main = table.find_all("tr")
     requirements = []
-    for elem in main:
-        ## Checks if elem is a course (clickable)
-        course = elem.find("td", class_="codecol")
-        if course != None:
-            change_space = course.text.strip().replace("\u00a0", " ") ## replace space symbol with space character
-            name = change_space.strip().replace("\u2013", "-")
-            ## checks if this is the parent is Area header or list comment
-            if len(requirements[-1].child) == 0: 
-                requirements[-1].child.append(Header("","Courses"))
-            ## checks if courses are relatted to the previous - such as or (chose one)
-            if 'or' in name:
-                ## only append when the previous is already containing other 'or' cases (in a list)
-                if type(requirements[-1].child[-1].child[-1]) == list:
-                    requirements[-1].child[-1].child[-1].append(name[3:])
+    try:
+        for elem in main:
+            ## Checks if elem is a course (clickable)
+            course = elem.find("td", class_="codecol")
+            if course != None:
+                change_space = course.text.strip().replace("\u00a0", " ") ## replace space symbol with space character
+                name = change_space.strip().replace("\u2013", "-")
+                ## checks if this is the parent is Area header or list comment
+                if len(requirements[-1].child) == 0: 
+                    requirements[-1].child.append(Header("","Courses"))
+                ## checks if courses are relatted to the previous - such as or (chose one)
+                if 'or' in name:
+                    ## only append when the previous is already containing other 'or' cases (in a list)
+                    if type(requirements[-1].child[-1].child[-1]) == list:
+                        requirements[-1].child[-1].child[-1].append(name[3:])
+                    else:
+                        prev = requirements[-1].child[-1].child[-1]
+                        requirements[-1].child[-1].child[-1] = [prev, name[3:]]
+                elif '-' in name:
+                    courses = expand_series(name)
+                    for elem in courses:
+                        requirements[-1].child[-1].child.append(elem)
                 else:
-                    prev = requirements[-1].child[-1].child[-1]
-                    requirements[-1].child[-1].child[-1] = [prev, name[3:]]
-            elif '-' in name:
-                courses = expand_series(name)
-                for elem in courses:
-                    requirements[-1].child[-1].child.append(elem)
-            else:
-                requirements[-1].child[-1].child.append(name)
-            continue
+                    requirements[-1].child[-1].child.append(name)
+                continue
 
-        area_header = elem.find("span", class_="courselistcomment areaheader")
-        if area_header != None:
-            change_space = area_header.text.strip().replace("\u00a0", " ")
-            name =  change_space.strip().replace("\u2013", "-")
-            requirements.append(Header(name, "Header"))
-            continue
+            area_header = elem.find("span", class_="courselistcomment areaheader")
+            if area_header != None:
+                change_space = area_header.text.strip().replace("\u00a0", " ")
+                name =  change_space.strip().replace("\u2013", "-")
+                requirements.append(Header(name, "Header"))
+                continue
 
-        list_comment = elem.find("span", class_="courselistcomment")
-        if list_comment != None:
-            change_space = list_comment.text.strip().replace("\u00a0", " ")
-            name =  change_space.strip().replace("\u2013", "-")
-            if len(requirements) == 0:
-                requirements.append(Header("", 'Header'))
-            requirements[-1].child.append(Header(name, 'Courses'))
+            list_comment = elem.find("span", class_="courselistcomment")
+            if list_comment != None:
+                change_space = list_comment.text.strip().replace("\u00a0", " ")
+                name =  change_space.strip().replace("\u2013", "-")
+                if len(requirements) == 0:
+                    requirements.append(Header("", 'Header'))
+                requirements[-1].child.append(Header(name, 'Courses'))
+        
+        return requirements  
     
-    return requirements  
-
+    except Exception:
+        print(url)
 
 def write_requirements_file(name, classes):
     """
@@ -176,7 +180,7 @@ def write_requirements_file(name, classes):
 
 def write_to_json(name, info):
     name = name.replace(' ', '_')
-    name = name.replace(',', '')
+    name = name.replace(',', '').replace('/', '-')
     with open('../data/' + name + 'json', 'w') as f:
         json_version = []
         for elem in info:
@@ -190,5 +194,6 @@ if __name__ == "__main__":
     all_websites = get_websites()
     for elem in all_websites:
         majorInfo = scrape_courses(elem[1])
-        write_to_json(elem[0], majorInfo)
-        ## write_requirements_file(each[0], course)
+        if majorInfo != None:
+            write_to_json(elem[0], majorInfo)
+        ##write_requirements_file(each[0], course)
--- scrape/translate.py
+++ scrape/translate.py
@@ -1,6 +1,6 @@
 import json 
 
-file_names = ["../data/holder.json"]
+file_names = ["../data/Global_Middle_East_Studies_B.A.json"]
 for elem in file_names: 
     with open(elem, 'r') as f: 
         data = json.load(f)
