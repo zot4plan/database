8b00114cfbb607aa0f9124ce42ff86f90ab5205e
Loc Linh <lockhong6395@gmail.com>
2022-04-30 12:07:52 -0700

    edited Quantitative Economics, Social Ecology; and Sociology

--- scrape/translate.py
+++ scrape/translate.py
@@ -1,5 +1,5 @@
 import json 
-file_names = ["../data/International_Studies_B.A.json"]
+file_names = ["../data/Quantitative_Economics_B.A.json"]
 for elem in file_names: 
     with open(elem, 'r') as f: 
         data = json.load(f)
