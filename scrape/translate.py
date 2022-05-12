53cbfdb7e5f13ffd882a077f8ee8fd1edb0d4a9b
Tram La <latb@uci.edu>
2022-05-12 16:32:16 -0700

    Bold header D-M

--- scrape/translate.py
+++ scrape/translate.py
@@ -1,5 +1,5 @@
 import json 
-file_names = ["../data/Criminology_Law_and_Society_B.A.json"]
+file_names = ["../data/Music_B.A.json"]
 for elem in file_names: 
     with open(elem, 'r') as f: 
         data = json.load(f)
