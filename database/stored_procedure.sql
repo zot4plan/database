70671ad3c399a4dbdd83a332aff3939ab2d85d81
loclinh <lockhong6395@gmail.com>
2022-06-27 18:28:15 -0700

    remove unused stored procedures

--- database/stored_procedure.sql
+++ database/stored_procedure.sql
@@ -18,27 +18,5 @@ FROM (SELECT DISTINCT(course_id)
                    WHERE program_id = p_id) as p LEFT JOIN courses ON courses.id = p.course_id; 
 END $$
 
-CREATE PROCEDURE get_all_ge() 
-BEGIN
-DECLARE finished INTEGER DEFAULT 0;
-DECLARE geId varchar(5) DEFAULT "false";
-DECLARE cursor_ge CURSOR FOR SELECT id FROM general_education;
-
-DECLARE CONTINUE HANDLER 
-FOR NOT FOUND SET finished = 1;
-
-SELECT id, name, note FROM general_education; 
-SELECT courses.* FROM courses JOIN (SELECT DISTINCT(course_id) 
-                   FROM courses_in_ge) as c ON courses.id = c.course_id; 
-
-OPEN cursor_ge;
-WHILE finished = 0 DO
-    FETCH cursor_ge INTO geId;
-    SELECT course_id from courses_in_ge where ge_id = geId;
-END WHILE;
-CLOSE cursor_ge;
-
-END $$
-
 DELIMITER ;
 
